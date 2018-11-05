d4e567fc95a475522d0c8610d438118529e8982f
Francis Williams <francis@fwilliams.info>
2018-11-05 14:38:19 -0500

    include normals in poisson_disk_sample

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py-sample-mesh",
-    version="1.0",
+    version="1.1",
     author="Francis Williams",
     author_email="francis@fwilliams.info",
     description="A Python Library for Generating Point Samples on Meshes",
--- src/sample.cpp
+++ src/sample.cpp
@@ -4,6 +4,8 @@
 #include <vcg/complex/algorithms/clustering.h>
 
 #include <fstream>
+#include <iostream>
+
 
 using namespace vcg;
 
@@ -14,19 +16,20 @@ struct MyUsedTypes : public UsedTypes<	Use<MyVertex>   ::AsVertexType,
                                         Use<MyEdge>     ::AsEdgeType,
                                         Use<MyFace>     ::AsFaceType>{};
 
-class MyVertex  : public Vertex<MyUsedTypes, vertex::Coord3f, vertex::Normal3f, vertex::BitFlags>{};
-class MyFace    : public Face< MyUsedTypes, face::FFAdj,  face::Normal3f, face::VertexRef, face::BitFlags > {};
+class MyVertex  : public Vertex<MyUsedTypes, vertex::Coord3f, vertex::Normal3f, vertex::BitFlags> {};
+class MyFace    : public Face<MyUsedTypes, face::FFAdj,  face::Normal3f, face::VertexRef, face::BitFlags> {};
 class MyEdge    : public Edge<MyUsedTypes>{};
-class MyMesh    : public tri::TriMesh< vector<MyVertex>, vector<MyFace> , vector<MyEdge>  > {};
+class MyMesh    : public tri::TriMesh<std::vector<MyVertex>, std::vector<MyFace>, std::vector<MyEdge>> {};
 
 
 /*
  * Copy a mesh stored as a #V x 3 matrix of vertices, V, and a #F x 3 matrix of face indices into a VCG mesh
  */
-template <typename DerivedV, typename DerivedF>
-static void vcg_mesh_from_vf(
+template <typename DerivedV, typename DerivedF, typename DerivedN>
+static void vcg_mesh_from_vfn(
     const Eigen::MatrixBase<DerivedV>& V,
     const Eigen::MatrixBase<DerivedF>& F,
+    const Eigen::MatrixBase<DerivedN>& N,
     MyMesh& m) {
 
   MyMesh::VertexIterator vit = Allocator<MyMesh>::AddVertices(m, V.rows());
@@ -34,38 +37,52 @@ static void vcg_mesh_from_vf(
   for (int i = 0; i < V.rows(); i++) {
     ivp[i] = &*vit;
     vit->P() = MyMesh::CoordType(V(i, 0), V(i, 1), V(i, 2));
+    if (N.rows() > 0) {
+      vit->N() = MyMesh::CoordType(N(i, 0), N(i, 1), N(i, 2));
+    }
     vit++;
   }
 
-  MyMesh::FaceIterator fit = Allocator<MyMesh>::AddFaces(m, F.rows());
-  for (int i = 0; i < F.rows(); i++) {
-    fit->V(0) = ivp[F(i, 0)];
-    fit->V(1) = ivp[F(i, 1)];
-    fit->V(2) = ivp[F(i, 2)];
-    fit++;
+  if (F.rows() > 0) {
+    MyMesh::FaceIterator fit = Allocator<MyMesh>::AddFaces(m, F.rows());
+    for (int i = 0; i < F.rows(); i++) {
+      fit->V(0) = ivp[F(i, 0)];
+      fit->V(1) = ivp[F(i, 1)];
+      fit->V(2) = ivp[F(i, 2)];
+      fit++;
+    }
   }
 
   tri::UpdateBounding<MyMesh>::Box(m);
 }
+
 
 /*
- * Copy a mesh stored as a #V x 3 matrix of vertices, V into a VCG mesh
+ * Copy a VCG mesh into a #V x 3 matrix of vertices, V
  */
-template <typename DerivedV>
-static void vcg_mesh_from_v(
-    const Eigen::MatrixBase<DerivedV>& V,
-    MyMesh& m) {
+template <typename DerivedV, typename DerivedN>
+static void vcg_mesh_to_vn(
+    const MyMesh& m,
+    Eigen::PlainObjectBase<DerivedV>& V,
+    Eigen::PlainObjectBase<DerivedN>& N) {
 
-  MyMesh::VertexIterator vit = Allocator<MyMesh>::AddVertices(m, V.rows());
-  std::vector<MyMesh::VertexPointer> ivp(V.rows());
-  for (int i = 0; i < V.rows(); i++) {
-    ivp[i] = &*vit;
-    vit->P() = MyMesh::CoordType(V(i, 0), V(i, 1), V(i, 2));
-    vit++;
+  V.resize(m.vn, 3);
+  N.resize(m.vn, 3);
+
+  int vcount = 0;
+  for (auto vit = m.vert.begin(); vit != m.vert.end(); vit++) {
+    V(vcount, 0) = vit->P()[0];
+    V(vcount, 1) = vit->P()[1];
+    V(vcount, 2) = vit->P()[2];
+
+    N(vcount, 0) = vit->N()[0];
+    N(vcount, 1) = vit->N()[1];
+    N(vcount, 2) = vit->N()[2];
+
+    vcount += 1;
   }
-
-  tri::UpdateBounding<MyMesh>::Box(m);
 }
+
 
 /*
  * Copy a VCG mesh into a #V x 3 matrix of vertices, V
@@ -111,6 +128,7 @@ A #pv x 3 matrix of points which are approximately evenly spaced and are a subse
 npe_function(poisson_disk_sample)
 npe_arg(v, dense_f32, dense_f64)
 npe_arg(f, dense_i32, dense_i64)
+npe_arg(n, dense_f32, dense_f64)
 npe_arg(radius, double)
 npe_default_arg(use_geodesic_distance, bool, false)
 npe_default_arg(best_choice_sampling, bool, false)
@@ -124,7 +142,7 @@ npe_begin_code()
   }
 
   MyMesh m;
-  vcg_mesh_from_vf(v, f, m);
+  vcg_mesh_from_vfn(v, f, n, m);
 
   MyMesh subM;
   tri::MeshSampler<MyMesh> mps(subM);
@@ -136,12 +154,15 @@ npe_begin_code()
   pp.geodesicDistanceFlag = use_geodesic_distance;
   tri::SurfaceSampling<MyMesh,tri::MeshSampler<MyMesh> >::PoissonDiskPruning(mps, m, radius, pp);
 
-  npe_Matrix_v ret;
-  vcg_mesh_to_v(subM, ret);
+  npe_Matrix_v ret_v;
+  npe_Matrix_n ret_n;
+  vcg_mesh_to_vn(subM, ret_v, ret_n);
 
   m.Clear();
   subM.Clear();
-  return npe::move(ret);
+  mps.reset();
+  return std::make_tuple(npe::move(ret_v), npe::move(ret_n));
+
 npe_end_code()
 
 
@@ -175,7 +196,9 @@ npe_begin_code()
   }
 
   MyMesh m;
-  vcg_mesh_from_v(v, m);
+  npe_Matrix_v n(0, 0);
+  Eigen::MatrixXi f(0, 0);
+  vcg_mesh_from_vfn(v, f, n, m);
 
   MyMesh cluM;
 
@@ -184,14 +207,14 @@ npe_begin_code()
   ClusteringGrid.AddPointSet(m);
   ClusteringGrid.ExtractMesh(cluM);
 
-  npe_Matrix_v ret;
-  vcg_mesh_to_v(cluM, ret);
-
+  npe_Matrix_v ret_v;
+  vcg_mesh_to_v(cluM, ret_v);
 
   m.Clear();
   cluM.Clear();
 
-  return npe::move(ret);
+  return npe::move(ret_v);
+
 npe_end_code()
 
 
@@ -213,6 +236,7 @@ A #pv x 3 matrix of samples
 npe_function(random_sample)
 npe_arg(v, dense_f32, dense_f64)
 npe_arg(f, dense_i32, dense_i64)
+npe_arg(n, dense_f32, dense_f64)
 npe_arg(num_samples, int)
 npe_doc(random_sample_doc)
 npe_begin_code()
@@ -224,19 +248,22 @@ npe_begin_code()
   }
 
   MyMesh m;
-  vcg_mesh_from_vf(v, f, m);
+  vcg_mesh_from_vfn(v, f, n, m);
 
   MyMesh rndM;
   tri::MeshSampler<MyMesh> mrs(rndM);
 
   tri::SurfaceSampling<MyMesh, tri::MeshSampler<MyMesh>>::VertexUniform(m, mrs, num_samples);
 
-  npe_Matrix_v ret;
-  vcg_mesh_to_v(rndM, ret);
+  npe_Matrix_v ret_v;
+  npe_Matrix_n ret_n;
+  vcg_mesh_to_vn(rndM, ret_v, ret_n);
+
 
   m.Clear();
   rndM.Clear();
 
-  return npe::move(ret);
+  return std::make_tuple(npe::move(ret_v), npe::move(ret_n));
+
 npe_end_code()
 
