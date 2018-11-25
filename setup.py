1269e3c7c5aa6c36605fcd51b741f5d1ea53995c
Francis Williams <francis@fwilliams.info>
2018-11-25 10:25:45 +0100

    Fix cmake to use sse3. We should be able to make a conda package that works everywhere now. Clean up setup.py script. Add setuptools artifacts to gitignore.

--- setup.py
+++ setup.py
@@ -2,25 +2,23 @@ import setuptools
 import os
 import re
 import sys
-import sysconfig
 import platform
 import subprocess
+import warnings
 
 from distutils.version import LooseVersion
-from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 
 
-class CMakeExtension(Extension):
+class CMakeExtension(setuptools.Extension):
     def __init__(self, name, sourcedir=''):
-        Extension.__init__(self, name, sources=[])
+        setuptools.Extension.__init__(self, name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
 
 
 class CMakeBuild(build_ext):
     def run(self):
         if os.path.exists('.git'):
-            #check_call(['git', 'submodule', 'init'])
             subprocess.check_call(['git', 'submodule', 'update', '--init', '--recursive'])
 
         try:
@@ -31,8 +29,7 @@ class CMakeBuild(build_ext):
                 ", ".join(e.name for e in self.extensions))
 
         if platform.system() == "Windows":
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)',
-                                         out.decode()).group(1))
+            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
             if cmake_version < '3.2.0':
                 raise RuntimeError("CMake >= 3.2.0 is required on Windows")
 
@@ -40,19 +37,18 @@ class CMakeBuild(build_ext):
             self.build_extension(ext)
 
     def build_extension(self, ext):
-        extdir = os.path.abspath(
-            os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir, '-DPYTHON_EXECUTABLE=' + sys.executable]
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
 
+        if cfg == 'Debug':
+            warnings.warn("Building extension %s in debug mode" % ext.name)
+
         if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
-                cfg.upper(),
-                extdir)]
-            if sys.maxsize > 2**32:
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            if sys.maxsize > 2 ** 32:
                 cmake_args += ['-A', 'x64']
             build_args += ['--', '/m']
         else:
@@ -60,23 +56,20 @@ class CMakeBuild(build_ext):
             build_args += ['--', '-j2']
 
         env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
-            env.get('CXXFLAGS', ''),
-            self.distribution.get_version())
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''), self.distribution.get_version())
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
-                              cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args,
-                              cwd=self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
         print()  # Add an empty line for cleaner output
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="point-cloud-utils",
-    version="0.1",
+    version="0.2.0",
     author="Francis Williams",
     author_email="francis@fwilliams.info",
     description="A Python Library of utilities for point clouds",
@@ -89,8 +82,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 2.7",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        ],
-    ext_modules=[CMakeExtension('sample_mesh')],
+    ],
+    ext_modules=[CMakeExtension('point_cloud_utils')],
     cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,
     install_requires=[
@@ -98,4 +91,3 @@ setuptools.setup(
         'scipy'
     ]
 )
-    
