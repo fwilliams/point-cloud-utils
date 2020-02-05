f02cb01c7cd92e067749395c72f89e720dd2bca5
Francis Williams <francis@fwilliams.info>
2020-02-04 19:54:15 -0500

    voronoi centroids

--- src/lloyd.cpp
+++ src/lloyd.cpp
@@ -156,6 +156,55 @@ void sample_tet_mesh_lloyd(const Eigen::MatrixBase<DerivedV> &V,
     P.transposeInPlace();
 }
 
+/*
+ * Sample a mesh using lloyd relaxation
+ */
+template <typename DerivedV, typename DerivedF, typename DerivedT, typename DerivedP>
+void voronoi_centroids(const Eigen::MatrixBase<DerivedV> &V,
+                     const Eigen::MatrixBase<DerivedF> &F,
+                     const Eigen::MatrixBase<DerivedT> &T,
+                     const Eigen::MatrixBase<DerivedP> &centers,
+                     Eigen::PlainObjectBase<DerivedP> &P) {
+
+    std::vector<double> centers_vec;
+    for (GEO::index_t i = 0; i < centers.rows(); i++) {
+        for (GEO::index_t j = 0; j < centers.cols(); j++) {
+            centers_vec.push_back(centers(i, j));
+        }
+    }
+    GEO::Mesh M;
+    vft_to_geogram_tet_mesh(V, F, T, M);
+
+    GEO::coord_index_t dimension_ = GEO::coord_index_t(M.vertices.dimension());
+    const std::string& delaunay = "default";
+    GEO::Delaunay_var delaunay_ = GEO::Delaunay::create(dimension_, delaunay);
+    GEO::RestrictedVoronoiDiagram_var RVD_ = GEO::RestrictedVoronoiDiagram::create(delaunay_, &M);
+    RVD_->set_volumetric(true);
+
+    std::vector<double> mg;
+    std::vector<double> m;
+    RVD_->set_check_SR(false);
+
+    P.resize(centers.rows(), centers.cols());
+
+    GEO::index_t nb_points = centers.rows();
+
+    mg.assign(nb_points * dimension_, 0.0);
+    m.assign(nb_points, 0.0);
+    delaunay_->set_vertices(nb_points, centers_vec.data());
+    RVD_->compute_centroids(mg.data(), m.data());
+    GEO::index_t cur = 0;
+    for(GEO::index_t j = 0; j < nb_points; j++) {
+        if(m[j] > 1e-30) {
+            double s = 1.0 / m[j];
+            for(GEO::index_t coord = 0; coord < dimension_; coord++) {
+                P(j, coord) = s * mg[cur + coord];
+            }
+        }
+        cur += dimension_;
+    }
+}
+
 // Put geogram log messages in the trash where they belong
 class GeoTrashCan: public GEO::LoggerClient {
 protected:
@@ -319,6 +368,77 @@ npe_begin_code()
   sample_tet_mesh_lloyd(V, F, T, n, num_lloyd, num_newton, P);
   Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> P3d;
   P3d = P;
+  return npe::move(P3d);
+
+npe_end_code()
+
+
+const char* voronoi_centroids_unit_cube_doc = R"Qu8mg5v7(
+Compute the centroids of a 3d voronoi diagram restricted to the unit cube [0, 1]^3
+
+Parameters
+----------
+centers: A [n, 3] array where each row is a point in a voronoi diagram restricted to
+         the unit cube [0, 1]^3
+
+Returns
+-------
+A [n, 3] array of centroids where each row is the centroid of the corresponding
+voronoi cell in the input centers.
+
+)Qu8mg5v7";
+npe_function(voronoi_centroids_unit_cube)
+npe_arg(centers, dense_float, dense_double)
+npe_doc(lloyd_3d_doc)
+npe_begin_code()
+
+  if (centers.maxCoeff() > 1.0 || centers.minCoeff() < 0.0) {
+    throw pybind11::value_error("Centers of voronoi diagram must lie in the unit cube [0, 1]^3");
+  }
+
+  init_geogram_only_once();
+
+  Eigen::MatrixXd V(9, 3);
+  V << 0. , 1. , 0. ,
+       0. , 0. , 1. ,
+       1. , 0. , 1. ,
+       1. , 0. , 0. ,
+       0. , 0. , 0. ,
+       1. , 1. , 1. ,
+       1. , 1. , 0. ,
+       0. , 1. , 1. ,
+       0.5, 0.5, 0.5;
+
+  Eigen::MatrixXi T(12, 4);
+  T << 8, 0, 7, 5,
+       6, 2, 5, 8,
+       3, 1, 2, 8,
+       0, 8, 7, 4,
+       7, 8, 5, 1,
+       8, 0, 5, 6,
+       3, 1, 8, 4,
+       2, 1, 5, 8,
+       8, 0, 6, 4,
+       7, 8, 1, 4,
+       6, 4, 3, 8,
+       2, 6, 3, 8;
+  Eigen::MatrixXi F(12, 3);
+  F << 0, 5, 7,
+       6, 2, 5,
+       3, 1, 2,
+       0, 7, 4,
+       7, 5, 1,
+       0, 6, 5,
+       3, 4, 1,
+       2, 1, 5,
+       0, 4, 6,
+       7, 1, 4,
+       6, 4, 3,
+       2, 6, 3;
+
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> ctrs_copy = centers.template cast<double>();
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> P3d;
+  voronoi_centroids(V, F, T, ctrs_copy, P3d);
   return npe::move(P3d);
 
 npe_end_code()
