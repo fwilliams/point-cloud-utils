db9a25c295da6b1cfcd1a7008b7091366a617197
Francis Williams <fwilliams@users.noreply.github.com>
2020-05-06 10:48:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ def main():
 
     setuptools.setup(
         name="point-cloud-utils",
-        version="0.8.0",
+        version="0.15.1",
         author="Francis Williams",
         author_email="francis@fwilliams.info",
         description="A Python Library of utilities for point clouds",
