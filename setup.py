9d9fdbb2dbd0c90a3f4764cb7786077acf3dc2a1
Francis Williams <francis@fwilliams.info>
2021-07-20 13:13:38 -0400

    bump version?

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ def main():
 
     setuptools.setup(
         name="point-cloud-utils",
-        version="0.18.2",
+        version="0.18.3",
         author="Francis Williams",
         author_email="francis@fwilliams.info",
         description="A Python Library of utilities for point clouds",
